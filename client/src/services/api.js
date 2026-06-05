f65c5089623cc7763507bda6995a27c01012b648
Yash-Bisht <whyrush711@gmail.com>
2026-06-05 09:53:54 +0530

    changed the baseUrl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://prepwie-ai.onrender.com/api",
 });
 
 api.interceptors.request.use(
